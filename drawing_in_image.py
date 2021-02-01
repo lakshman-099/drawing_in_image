d0f174f9ebfd9243abad07e64489dcbec5011539
LAKSHMAN POKURI <60040041+lakshman-099@users.noreply.github.com>
2021-02-01 11:30:01 +0530

    Add files via upload

---
+++ drawing_in_image.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img=cv2.imread('C:/Users/lakki/Pictures/Camera Roll/photo(2).jpg', 1)
+img=cv2.line(img, (0,0), (255,255), (255,0,0), 15)
+img=cv2.arrowedLine(img, (0,255), (255,255), (255,0,0), 10)
+img=cv2.rectangle(img, (304, 0), (510, 128), (0,0,255), 10)
+img=cv2.circle(img, (447,63), 63, (0,0,255), -1)
+font=cv2.FONT_HERSHEY_SIMPLEX
+img=cv2.putText(img, 'Lakshman', (10, 500), font, 4, (255,255,255))
+cv2.imshow('test', img)
+plt.imshow(img)
+plt.show()
